51ce4bb10c40bcbdad472095bef1a53c42feada1
AaronCole700 <45340039+AaronCole700@users.noreply.github.com>
2018-11-25 14:08:22 -0600

    Initial Commit

---
+++ ClientApp/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MaterialModule } from './material/material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TestComponent } from './test/test.component';
+import { NavComponent } from './nav/nav.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TestComponent,
+    NavComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MaterialModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
